9d46de5a1ca4ef811332687c8925865a6015385d
George Shuklin <george.shuklin@gmail.com>
2019-07-04 09:17:21 +0300

    Color change on redraw

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate gtk;
 extern crate gio;
 extern crate gdk;
+extern crate rand;
 
 use gtk::prelude::*;
 use gio::prelude::*;
@@ -9,6 +10,8 @@ use cairo;
 use gtk::{BoxExt, ContainerExt, DrawingArea, ScrolledWindowExt, StateFlags, WidgetExt};
 use std::cell::RefCell;
 use gdk::{WindowExt};
+use rand::Rng;
+
 
 fn app( app:&gtk::Application) {
         let win = gtk::ApplicationWindow::new(app);
@@ -17,15 +20,20 @@ fn app( app:&gtk::Application) {
         win.set_title("Basic example");
         let frame = gtk::Frame::new(None);
         let area = DrawingArea::new();
+        let mut rng = RefCell::new(rand::thread_rng());
+        // let clo = move ||{rng.gen_range(0.0, 1.0)};
         area.connect_draw(move|w, c|{
             println!("w: {} c:{}",w, c);
+            let r = rng.borrow_mut().gen_range(0.0, 1.0);
+            let g = rng.borrow_mut().gen_range(0.0, 1.0);
+            let b = rng.borrow_mut().gen_range(0.0, 1.0);
+            c.set_source_rgb(r, g, b);
             c.rectangle(1.0, 1.0, 100.0, 200.0);
             c.fill();
             gtk::Inhibit(false)
         });
         frame.add(&area);
         win.add(&frame);
-        // gtk::container_add(win, frame);
         win.show_all();
 }
 
