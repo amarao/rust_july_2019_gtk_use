1e5d924dc56e3f2f08315c0a68866a37481afe02
George Shuklin <george.shuklin@gmail.com>
2019-07-04 08:26:22 +0300

    First draw

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 extern crate gtk;
 extern crate gio;
+extern crate gdk;
 
 use gtk::prelude::*;
 use gio::prelude::*;
 use std::env;
+use cairo;
+use gtk::{BoxExt, ContainerExt, DrawingArea, ScrolledWindowExt, StateFlags, WidgetExt};
+use std::cell::RefCell;
+use gdk::{WindowExt};
 
 fn app( app:&gtk::Application) {
         let win = gtk::ApplicationWindow::new(app);
 
         win.set_default_size(1920, 1080);
         win.set_title("Basic example");
-
+        let frame = gtk::Frame::new(None);
+        let area = DrawingArea::new();
+        area.connect_draw(move|w, c|{
+            println!("w: {} c:{}",w, c);
+            c.rectangle(1.0, 1.0, 100.0, 200.0);
+            c.fill();
+            gtk::Inhibit(false)
+        });
+        frame.add(&area);
+        win.add(&frame);
+        // gtk::container_add(win, frame);
         win.show_all();
 }
 
@@ -19,5 +34,6 @@ fn main() {
                                       gio::ApplicationFlags::FLAGS_NONE)
                                  .expect("Application::new failed");
     uiapp.connect_activate(app);
+
     uiapp.run(&env::args().collect::<Vec<_>>());
 }
